c8d6b08e72e3b32267c72d9b733aef8bc749dd6a
self <sqshanyao@163.com>
2018-12-18 17:47:41 +0800

    change url

--- backend/love/src/main/java/com/love/controller/agree/AgreeController.java
+++ backend/love/src/main/java/com/love/controller/agree/AgreeController.java
@@ -3,13 +3,17 @@ package com.love.controller.agree;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/love/lilac")
 @Controller
 public class AgreeController {
 
-    public void agree(@RequestBody String json) {
-        System.out.println(json);
+    @ResponseBody
+    public boolean agree(@RequestParam(value = "agree", required = true) String agree) {
+        System.out.println(agree);
+        return true;
     }
 
 }
