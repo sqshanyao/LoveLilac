f858890ca6939f1e9fac98720c50f58220b182ac
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-12-24 23:41:40 +0800

    update

---
+++ backend/love/src/main/java/com/love/bean/LoveConstant.java
@@ -0,0 +1,8 @@
+package com.love.bean;
+
+public class LoveConstant {
+    public static final String agree = "yes";
+    public static final String refuse = "no";
+    public static final String no_answer = "no_answer";
+
+}
--- backend/love/src/main/java/com/love/controller/agree/AgreeController.java
+++ backend/love/src/main/java/com/love/controller/agree/AgreeController.java
@@ -1,19 +1,29 @@
 package com.love.controller.agree;
 
+import com.love.bean.LoveConstant;
+import com.love.domain.CommonConfig;
+import com.love.service.AgreeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/love/lilac")
 @Controller
 public class AgreeController {
+    @Autowired
+    AgreeService agreeService;
 
     @ResponseBody
-    public boolean agree(@RequestParam(value = "agree", required = true) String agree) {
-        System.out.println(agree);
-        return true;
+    @PostMapping(value = "/agree")
+    public String agree() {
+        CommonConfig commonConfig = agreeService.getByType(1);
+        if (commonConfig == null) {
+            return  LoveConstant.no_answer;
+        }else if (LoveConstant.agree.equals(commonConfig.getValue())) {
+            return LoveConstant.agree;
+        } else {
+            return LoveConstant.refuse;
+        }
     }
 
 }
