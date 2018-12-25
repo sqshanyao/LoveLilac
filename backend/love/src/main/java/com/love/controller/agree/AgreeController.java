5ef235909dfbbbc38a1cf33d9308981dfe3d4db3
self <sqshanyao@163.com>
2018-12-25 10:41:06 +0800

    no message

--- backend/love/src/main/java/com/love/controller/agree/AgreeController.java
+++ backend/love/src/main/java/com/love/controller/agree/AgreeController.java
@@ -19,7 +19,7 @@ public class AgreeController {
     @PostMapping(value = "/agree")
     public String agree() {
         Logger logger = LoggerFactory.getLogger(this.getClass());
-        String result = "";
+        String result;
         CommonConfig commonConfig = agreeService.getByType(1);
         if (commonConfig == null) {
             result  = LoveConstant.no_answer;
--- backend/love/src/test/java/com/love/controller/LoginControllerTest.java
+++ backend/love/src/test/java/com/love/controller/LoginControllerTest.java
@@ -1,4 +1,15 @@
 package com.love.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LoginControllerTest {
+    @Test
+    public void agreeTest() {
+
+    }
 }
